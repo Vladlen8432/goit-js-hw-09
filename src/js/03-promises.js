c1d4197a268b351fb80afb45e71e4aa414465492
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-09-05 23:20:14 +0300

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,3 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
