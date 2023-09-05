978cae022358f729a08dece5f3d2fd66488c71fd
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-09-05 23:12:53 +0300

    promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const form = document.querySelector('.form');
+  const delayInput = form.querySelector('input[name = "delay"]');
+  const stepInput = form.querySelector('input[name = "step"]');
+  const amountInput = form.querySelector('input[name = "amount"]');
+
+  const initialDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = initialDelay + i * step;
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleFormSubmit);
